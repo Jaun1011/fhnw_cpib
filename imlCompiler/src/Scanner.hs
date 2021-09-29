ec1c3ff1f8ae5e3858b5354e7926f67acbd33568
jku <jan@jku.one>
2021-09-30 00:27:38 +0200

    literal logic add

--- imlCompiler/src/Scanner.hs
+++ imlCompiler/src/Scanner.hs
@@ -2,10 +2,12 @@ module Scanner
     ( scanner
     ) where
 
-import Prelude (Show, Maybe (Just), String, Int)
+import Prelude (Show, Maybe (Just, Nothing), String, Int, snd, fst, ($), otherwise)
+import GHC.Unicode (isAlpha, isDigit)
+import Data.Sequence.Internal.Sorting (QList(Nil))
 
-data Terminal 
-    = IDENT 
+data Terminal
+    = IDENT
     | NOT
     | LPAREN
     | RPAREN
@@ -16,18 +18,20 @@ data Terminal
     | IF
     | THEN
     | ELSE
+    | LITERAL
     deriving(Show)
 
 data Attirbute
-    = RelOperator RelOperator 
-    | IntegarType Int   
+    = RelOperator RelOperator
+    | IntegarType Int
+    | StringType String
     deriving(Show)
 
-data RelOperator 
-    = GREATER 
+data RelOperator
+    = GREATER
     | LESS
-    | EQUAL 
-    | GREATER_EQUAL 
+    | EQUAL
+    | GREATER_EQUAL
     | LESS_EQUAL
     deriving(Show)
 
@@ -39,12 +43,32 @@ scanner :: String -> [Token]
 scanner = s0
 
 s0 :: String -> [Token]
-s0 ('<':'=':cs) = (RELOPR, Just (RelOperator LESS_EQUAL)) : s0 cs
+s0 [] = []
+s0 ('<':'=':cs) = (RELOPR, Just (RelOperator LESS_EQUAL))    : s0 cs
 s0 ('>':'=':cs) = (RELOPR, Just (RelOperator GREATER_EQUAL)) : s0 cs
-s0 ('=':cs) = (RELOPR, Just (RelOperator EQUAL)) : s0 cs
-s0 ('<':cs) = (RELOPR, Just (RelOperator LESS)) : s0 cs
-s0 ('>':cs) = (RELOPR, Just (RelOperator GREATER)) : s0 cs
-s0 (c:cs) = s0 cs
-s0 "" = []
+s0 ('=':cs)     = (RELOPR, Just (RelOperator EQUAL))         : s0 cs
+s0 ('<':cs)     = (RELOPR, Just (RelOperator LESS))          : s0 cs
+s0 ('>':cs)     = (RELOPR, Just (RelOperator GREATER))       : s0 cs
+s0 (c:cs)
+    | isAlpha c =
+        let x = s1 (c:cs)
+        in snd x : s0 (fst x)
+
+
+s1 :: String -> (String, Token)
+s1 x = ("", (LITERAL, Just (StringType $s1_literal' x)))
+
+
+s1_remain' :: String -> String
+s1_remain' (a:as)
+    | isAlpha a = s1_remain' as
+    | isDigit a = s1_remain' as
+    | otherwise  = as
+
+s1_literal' :: String -> String
+s1_literal' (a:as)
+    | isAlpha a = a : s1_literal' as
+    | isDigit a = a : s1_literal' as
+    | otherwise  = ""
 
 
