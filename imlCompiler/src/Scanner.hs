050bcf71e8e19df5b30bbbfb3f80b1a5aa1047e1
jku <jan@jku.one>
2021-09-30 13:58:18 +0200

    add numbers to scanner

--- imlCompiler/src/Scanner.hs
+++ imlCompiler/src/Scanner.hs
@@ -2,7 +2,7 @@ module Scanner
     ( scanner
     ) where
 
-import Prelude (Show, Maybe (Just, Nothing), String, Int, snd, fst, ($), otherwise, (||), Char, Bool, Eq ((==)))
+import Prelude (Show, Maybe (Just, Nothing), String, Int, snd, fst, ($), otherwise, (||), Char, Bool, Eq ((==)), read)
 import GHC.Unicode (isAlpha, isDigit)
 import Data.Sequence.Internal.Sorting (QList(Nil))
 
@@ -19,11 +19,12 @@ data Terminal
     | THEN
     | ELSE
     | LITERAL
+    | ALITERAL
     deriving(Show)
 
 data Attirbute
     = RelOperator RelOperator
-    | IntegarType Int
+    | IntType Int
     | StringType String
     deriving(Show)
 
@@ -53,6 +54,9 @@ s0 (c:cs)
     | isAlpha c =
         let x = s1 (c:cs)
         in snd x : s0 (fst x)
+    | isDigit c = 
+        let x = s2 (c:cs)
+        in snd x : s0 (fst x)
 
 
 s1 :: String -> (String, Token)
@@ -60,10 +64,15 @@ s1 x =
     let n = split x isLiteral 
     in (snd n, (LITERAL, Just (StringType (fst n))))
 
+s2 :: String -> (String, Token)
+s2 x = 
+    let n = split x isDigit
+    in (snd n, (ALITERAL, Just (IntType (read (fst n) :: Int)))) -- todo: jku int needs to be fixed for larger numbers
+
+
 
 isLiteral :: Char -> Bool 
 isLiteral c = isDigit c || isAlpha c
-
 
 -- split string by condition in (head, tail)
 split :: String -> (Char -> Bool) -> (String , String)
@@ -73,5 +82,6 @@ split (a:as) fn
          let n = split as fn 
          in (a : fst n, snd n)
     | otherwise = ("", a:as)
+
 
 
