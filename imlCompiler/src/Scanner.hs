79b87d626f14ee2b6e80b96ba0f3b409a37c3184
Ismail_Cadaroski <ismail.cadaroski@students.fhnw.ch>
2021-10-03 18:44:52 +0200

    Update Scanner.hs
    - all  the listed keywords defined
    - all the special char cases inserted and the unsure ones commented out if they are needed in a later iteration

--- imlCompiler/src/Scanner.hs
+++ imlCompiler/src/Scanner.hs
@@ -6,30 +6,59 @@ import Prelude (Show, Maybe (Just, Nothing), String, Int, snd, fst, ($), otherwi
 import GHC.Unicode (isAlpha, isDigit)
 import GHC.Float (fromRat'')
 import Data.Maybe
+import Language.Haskell.TH.Syntax (Callconv)
+import Foreign (Int32)
+import Data.Int (Int64)
 
 data Terminal
     = IDENT
+    | BOOL
+    | CALL
+    | CONST
+    | COPY
+    | DEBUGIN
+    | DEBUGOUT
+    | DO
+    | ENDFUN
+    | ENDIF
+    | ENDPROC
+    | ENDPROGRAM
+    | ENDWHILE
+    | FALSE
+    | FUN
+    | GLOBAL
+    | IF
+    | IN
+    | INIT
+    | INOUT
+    | LOCAL
+    | OUT
+    | PROC
+    | PROGRAM
+    | REF
+    | RETURNS
+    | SKIP
+    | THEN
+    | TRUE
+    | VAR
+    | WHILE
     | COMMENT
     | ASSIGN
-    | NOT
     | LPAREN
     | RPAREN
     | BECOMES
     | SEMICOLON
     | COMMA
     | RELOPR
-    | WHILE
     | LOGICOPR
     | ARITMOPR
-    | SKIP
-    | IF
-    | THEN
+    | DIVOPR
     | ELSE
-    | ENDIF
     | LITERAL
     | ALITERAL
     | TYPEDEF
     | UNKNOWN
+    | MODE
     
     deriving(Show)
 
@@ -37,16 +66,21 @@ data Attirbute
     = RelOperator RelOperator
     | AritmeticOperator AritmeticOperator
     | LogicOperator LogicOperator
+    | DivOperator DivOperator
     | IntType Int
+
+    | Int1024 Int
+    | Int32 Int32
+    | Int64 Int64
+    
     | StringType String
     deriving(Show)
-
-
-
 
 data LogicOperator
     = AND
     | OR
+    | XOR
+    | NOT
     deriving(Show)
 
 data RelOperator
@@ -55,6 +89,7 @@ data RelOperator
     | EQUAL
     | GREATER_EQUAL
     | LESS_EQUAL
+    | NOT_EQUAL
     deriving(Show)
 
 
@@ -63,8 +98,18 @@ data AritmeticOperator
    | MINUS
    | MULTI
    | DIV
-    deriving(Show)
+   deriving(Show)
 
+
+data DivOperator
+    = DIV_E
+   | DIV_F
+   | DIV_T
+   | MOD_E
+   | MOD_F
+   | MOD_T
+   deriving(Show)
+ 
 
 type Token = (Terminal, Maybe Attirbute)
 
@@ -84,14 +129,55 @@ kwt ((LITERAL, attr):as) = keyword (litAttr $ fromJust attr) : kwt as
 kwt (a:as) = a: kwt as
 
 
-
 keyword :: String -> Token
 keyword "if"    = (IF,Nothing)
 keyword "then"  = (THEN,Nothing)
 keyword "else"  = (ELSE,Nothing)
 keyword "endif" = (ENDIF,Nothing)
 keyword "while" = (WHILE,Nothing)
+keyword "bool"  = (BOOL,Nothing)
+keyword "call"  = (CALL, Nothing)
+keyword "const" = (CONST, Nothing)
+keyword "copy"  = (COPY, Nothing)
+keyword "debugin" = (DEBUGIN, Nothing)
+keyword "debugout" =  (DEBUGOUT, Nothing)
+keyword "do"    = (DO, Nothing)
+keyword "endfun"= (ENDFUN, Nothing)
+keyword "endproc" = (ENDPROC, Nothing)
+keyword "endprogram" = (ENDPROGRAM, Nothing)
+keyword "endwhile" = (ENDWHILE, Nothing)
+keyword "false" = (LITERAL, Nothing)
+keyword "fun"   = (FUN, Nothing)
+keyword "global"= (GLOBAL, Nothing)
+keyword "in"    = (IN, Nothing)
+keyword "init"  = (INIT, Nothing)
+keyword "inout" = (INOUT, Nothing)
+keyword "local" = (LOCAL, Nothing)
+keyword "modE"  = (DIVOPR, Just(DivOperator MOD_E)) 
+keyword "out"   = (OUT, Nothing)
+keyword "proc"  = (PROC, Nothing)
+keyword "program" = (PROGRAM, Nothing)
+keyword "ref"   = (REF, Nothing)
+keyword "returns" = (RETURNS, Nothing)
+keyword "skip"  = (SKIP, Nothing)
+keyword "true"  = (TRUE, Nothing)
+keyword "var"   = (VAR, Nothing)
+keyword "comment" = (COMMENT, Nothing)
+keyword "assign"= (ASSIGN, Nothing)
+keyword "lparen"= (LPAREN, Nothing)
+keyword "rparen"= (RPAREN, Nothing)
+keyword "becomes"= (BECOMES, Nothing)
+keyword "semicolon"= (SEMICOLON, Nothing)
+keyword "comma" = (COMMA, Nothing)
+keyword "relopr" = (RELOPR, Nothing)
+keyword "logicopr" = (LOGICOPR, Nothing)
+keyword "aritmopr" = (ARITMOPR, Nothing)
+keyword "literal" = (LITERAL, Nothing)
+keyword "aliteral"= (ALITERAL, Nothing)
+keyword "typedef"= (TYPEDEF, Nothing)
+keyword "unknown"= (UNKNOWN, Nothing)
 keyword c       = (LITERAL, Just $StringType c) 
+
 
 
 -- 
@@ -105,6 +191,8 @@ s0 ('/':'/':cs) = let (s, token) = s1_comment cs in token: s0 s
 s0 (':':'=':cs) = (ASSIGN, Nothing) : s0 cs
 
 s0 ('>':'=':cs) = (RELOPR, Just (RelOperator GREATER_EQUAL))    : s0 cs
+s0 ('/': '=':cs)= (RELOPR, Just (RelOperator NOT_EQUAL))        : s0 cs
+
 s0 ('=':cs)     = (RELOPR, Just (RelOperator EQUAL))            : s0 cs
 s0 ('<':cs)     = (RELOPR, Just (RelOperator LESS))             : s0 cs
 s0 ('>':cs)     = (RELOPR, Just (RelOperator GREATER))          : s0 cs
@@ -120,6 +208,26 @@ s0 (')':cs)     = (RPAREN, Nothing)     : s0 cs
 s0 (':':cs)     = (TYPEDEF, Nothing)    : s0 cs
 s0 (';':cs)     = (SEMICOLON, Nothing)  : s0 cs
 s0 (',':cs)     = (COMMA, Nothing)      : s0 cs
+
+s0 ('!':cs)     = (DEBUGOUT, Nothing)   : s0 cs
+s0 ('?':cs)     = (DEBUGIN, Nothing)    : s0 cs
+--0 ('"':cs)     = (, Nothing)      : s0 cs
+--s0 ('#':cs)     = (Nothing, Nothing)      : s0 cs
+--s0 ('$':cs)     = (Nothing, Nothing)      : s0 cs
+s0 ('%':cs)     = (MODE, Nothing)       : s0 cs
+s0 ('&':cs)     = (LOGICOPR, Just (LogicOperator AND))      : s0 cs
+--s0 ('`':cs)     = (Nothing, Nothing)      : s0 cs
+--s0 ('_':cs)     = (Nothing, Nothing)      : s0 cs
+--s0 ('.':cs)     = (Nothing, Nothing)      : s0 cs
+--s0 ('@':cs)     = (Nothing, Nothing)      : s0 cs
+--s0 ('[':cs)     = (Nothing, Nothing)      : s0 cs
+--s0 (']':cs)     = (Nothing, Nothing)      : s0 cs
+--s0 ('\\':cs)     = (Nothing, Nothing)      : s0 cs
+s0 ('^':cs)     = (LOGICOPR, Just (LogicOperator XOR))      : s0 cs
+--s0 ('{':cs)     = (Nothing, Nothing)      : s0 cs
+--s0 ('}':cs)     = (Nothing, Nothing)      : s0 cs
+s0 ('|':cs)     = (LOGICOPR, Just (LogicOperator OR))      : s0 cs
+s0 ('~':cs)     = (LOGICOPR, Just (LogicOperator NOT))      : s0 cs
 
 s0 (' ':cs)     = s0 cs
 s0 ('\n':cs)    = s0 cs
