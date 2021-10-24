91701064522c5e1882691c9a58a322460a45b864
Ismail_Cadaroski <ismail.cadaroski@students.fhnw.ch>
2021-10-24 17:25:32 +0200

    Update ParsingLib.hs
    Update / rewrote ParsingLib and added comments

--- src/ParsingLib.hs
+++ src/ParsingLib.hs
@@ -4,100 +4,98 @@ module ParsingLib (module ParsingLib, module Control.Applicative) where
 
 import Control.Applicative
 import Data.Char
+import Scanner
+import Model
+import GHC.TypeLits (AppendSymbol)
 
--- Basic definitions
 
-newtype Parser a = P (String -> [(a,String)])
+-- Takes a string and returns a maybe of  "a" and the string which wasn't consumed
+newtype Parser a = P { parse :: String -> Maybe (a, String)}
 
-parse :: Parser a -> String -> [(a,String)]
-parse (P p) = p
 
-item :: Parser Char
-item = P (\inp -> case inp of
-                     []     -> []
-                     (x:xs) -> [(x,xs)])
-
--- Sequencing parsers
-
+-- Functor needed to map multiple parser at each other
 instance Functor Parser where
    -- fmap :: (a -> b) -> Parser a -> Parser b
    fmap g p = P (\inp -> case parse p inp of
-                            []        -> []
-                            [(v,out)] -> [(g v, out)])
+                           Nothing     -> Nothing 
+                           Just (a, out) -> Just (g a, out))
 
+-- applicative instace to chain parsers
 instance Applicative Parser where
-   -- pure :: a -> Parser a
-   pure v = P (\inp -> [(v,inp)])
+   --pure :: a -> Parser a
+   pure v = P (\inp -> Just (v, inp))
+   -- <*> :: Parser (a->b) -> Parser a -> Parser b
+   pg <*> pa = P (\inp -> 
+      case parse pg inp of
+         Nothing     -> Nothing
+         Just (g,out)-> case parse pa out of
+                           Nothing        -> Nothing 
+                           Just (a, out') -> Just (g a, out'))
 
-   -- <*> :: Parser (a -> b) -> Parser a -> Parser b
-   pg <*> px = P (\inp -> case parse pg inp of
-                             []        -> []
-                             [(g,out)] -> parse (fmap g px) out)
-
-instance Monad Parser where
-   -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
-   p >>= f = P (\inp -> case parse p inp of
-                           []        -> []
-                           [(v,out)] -> parse (f v) out)
-
+-- alternative has two methods "empty" and "choice -> takes 2 things an chooses one"
 -- Making choices
-
-instance Alternative Parser where
-   -- empty :: Parser a
-   empty = P (\inp -> [])
-
+instance Alternative Parser where 
+   --empty :: Parser a
+   empty = P (\inp -> Nothing )
    -- (<|>) :: Parser a -> Parser a -> Parser a
-   p <|> q = P (\inp -> case parse p inp of
-                           []        -> parse q inp
-                           [(v,out)] -> [(v,out)])
+   -- try either p or q if p works don't look at q
+   p <|> q = P (\inp -> 
+      case parse p inp of 
+         Nothing       -> parse q inp   -- If Parser p fails
+         Just (v, out) -> Just (v,out)) -- If parser p succeeds
 
--- Derived primitives
+-- monad is used to remove case based code duplication, basically composition of even func
+instance Monad Parser where
+    -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+    pa >>= f = P (\inp -> 
+       case parse pa inp of
+          Nothing      -> Nothing 
+          Just (a, rest) -> parse (f a) rest)
 
-sat :: (Char -> Bool) -> Parser Char
-sat p = do x <- item
-           if p x then return x else empty
+-- parses a Char if predicate p is evaluated as true
+sat :: (Char -> Bool ) -> Parser Char 
+sat p = do 
+   -- parses item and gets access to char
+   c <- item
+   -- if the char = predikate give pure c back or with empty the failed parser
+   if p c then return c else empty
 
+-- parses any char
+item :: Parser Char
+item = P (\inp -> case inp of
+                     (c:cs) -> Just (c, cs)
+                     _      -> Nothing)
+
+-- parses only diggits
 digit :: Parser Char
 digit = sat isDigit
 
+-- parses a char if it matches the given c
 char :: Char -> Parser Char
-char x = sat (== x)
+char c = sat (==c)
 
+-- parses natural numbers string to int values
+nat :: Parser Int 
+nat = fmap read (some digit)
+
+-- parses to whole numbers -> takes the char and parses + negates the '-'
+-- (whole numbers are a nat or a mit with '-' beforehand)
+int :: Parser Int 
+int = nat 
+    <|> fmap (*(-1)) (char '-' *> nat)
+
+-- builds a parser that consumes the defined string
 string :: String -> Parser String
-string []     = return []
-string (x:xs) = do char x
-                   string xs
-                   return (x:xs)
-
-
-
-nat :: Parser Int
-nat = do xs <- some digit
-         return (read xs)
-
-int :: Parser Int
-int = do char '-'
-         n <- nat
-         return (-n)
-       <|> nat
+string []      = pure []
+string (c:cs)  = pure (:) <*> char c <*> string cs
 
 -- Handling spacing
-
 space :: Parser ()
-space = do many (sat isSpace)
-           return ()
+space = fmap (\_ -> ()) (many (sat isSpace))
 
+-- token Is a primitive that ignores any space before and after p
 token :: Parser a -> Parser a
-token p = do space
-             v <- p
-             space
-             return v
+token p = pure (\_ r _ -> r) <*> space <*> p <*> space
 
-natural :: Parser Int
-natural = token nat
 
-integer :: Parser Int
-integer = token int
 
-symbol :: String -> Parser String
-symbol xs = token (string xs)
