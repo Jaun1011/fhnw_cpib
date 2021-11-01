7129f8a6cd878b4b77cab99bbbc7209416b969cf
Ismail_Cadaroski <ismail.cadaroski@students.fhnw.ch>
2021-11-01 21:57:56 +0100

    Update Parser.hs
    startet the tree construction

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,22 +1,59 @@
 import ParsingLib
 
+
 --expr ::= term expr + | term
 --term ::= factor * term | factor
 --factor ::= (expr) | int
+
+inat :: Parser IExpr 
+inat = do ds <- some digit
+          return (IConst (read ds))
+
+
+
+expr :: Parser IExpr
 expr = do x <- term
           char '+'
           y <- expr
-          return (x+y)
+          return (IAdd x y)
         <|> term
 
+term :: Parser IExpr
 term = do x <- factor
           char '*'
           y <- term
-          return (x*y)
+          return (IMul x y)
         <|> factor
 
 factor = do char '('
             x <- expr
             char ')'
             return x
-           <|> int
+           <|> inat
+
+data BoolExpr = 
+  BConst Bool
+  | BTrue BoolExpr
+  | BFalse BoolExpr
+  deriving Show
+
+data CondExpr = 
+  IF CondExpr CondExpr (Maybe CondExpr)
+  | IFELSE CondExpr CondExpr 
+
+
+
+data IExpr = 
+  IConst Int 
+  | IMul IExpr IExpr
+  | IAdd IExpr IExpr 
+  deriving Show
+
+a :: IExpr
+a = IAdd (IMul (IConst 2) (IConst 3)) (IConst 4)
+
+eval :: IExpr -> Int 
+eval (IConst i) = i
+eval (IMul lhs rhs) = eval lhs * eval rhs
+eval (IAdd lhs rhs) = eval lhs + eval rhs
+
