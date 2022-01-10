c9ddff8d6be795281c57bed482567895af222bb4
jku <jan.kuonen.93@gmail.com>
2022-01-11 00:33:11 +0100

    fix issues local storage

--- src/CodeGenerator.hs
+++ src/CodeGenerator.hs
@@ -34,12 +34,16 @@ type FramePointer   = Int
 
 type Storage = (StackPointer, ExtremePointer, ProgramPointer, FramePointer)
 
+addProgPointer :: Storage -> [a] -> Storage
+addProgPointer (a,b,c,d) c2 = (a,b,c + length c2,d)
+
+
 genCode :: IDecl -> ([Instruction], [Symbol])
 genCode (IProg id _ glob cmd) = (allins ++ cmdins ++ [Stop], symalc)
     where 
             sym = createSymbols glob id
             (allins, symalc, st) = allocC sym env (0,0,0,0) glob
-            cmdins = cmdC symalc env cmd (length allins)
+            cmdins = cmdC symalc env st cmd  (length allins)
 
             env = ""
 
@@ -48,7 +52,7 @@ genCode (IStore  _ _) =  ([],[])
 genCode _ =  ([],[])
 
 
-allocC ::  [Symbol] -> Env -> Storage -> IDecl -> ([Instruction], [Symbol], Storage)
+allocC ::  [Symbol] -> Env -> Storage->  IDecl -> ([Instruction], [Symbol], Storage)
 allocC sym env store decl =  alloc sym decl store
         where 
                 alloc sym (IDeclItem a b) store = (ia ++ ib,  symb, st2)
@@ -56,55 +60,58 @@ allocC sym env store decl =  alloc sym decl store
                                         (ia, syma, st1) = alloc sym a store
                                         (ib, symb, st2) = alloc syma b st1
                 alloc sym INoDecl t = ([],sym,t)
-               
-                alloc sym (IStore _ (IArrayType id expr _)) (st,e,p,f) =
+                
+                alloc sym (IStore _ (IArrayType id expr _)) (st,e,p,f) = 
                                 (
                                  instExp ++ instComp
                                 ,setAddressById sym (st + 1, storeId id)
-                                ,(st + maxarraysize + 1 ,e, p + 1,f))
+                                ,(st + maxarraysize + 1,e, p + 1,f))
 
                     where 
-                        addrLen   = st
+                        addrLen   = st 
                         addrArray = addrLen + 1
 
                         instExp = [AllocBlock 1
-                                  ,LoadIm IntVmTy (IntVmVal addrLen)]
-                                 
-                                  ++ oprR sym env expr
+                                  ,LoadIm IntVmTy (IntVmVal addrLen)] 
+                                  
+                                  ++ oprR sym env (p + 2) expr 
                                   ++ [Store]
+                                  ++ [LoadIm IntVmTy (IntVmVal addrLen)
+                                     ,Deref]
+
 
                         instComp = [LoadIm Int1024VmTy (Int1024VmVal $Int1024 $toInteger maxarraysize)
                                    ,Gt Int1024VmTy
-                                   ,CondJump sizeCond
-                                   ,Output (IntTy WL1024) $"array is bigger than maxsize = " ++ show maxarraysize
+                                   ,CondJump $sizeCond - 1
+                                   ,Output (IntTy WL1024) $"[Runtime Exception] array is bigger than maxsize = " ++ show maxarraysize
                                    ,Stop
-                                   ,AllocBlock maxarraysize
+                                   ,AllocBlock $maxarraysize
                                    ]
 
                         sizeCond = p + length instExp  + length instComp
-                        maxarraysize = 1
+                        maxarraysize = 10
 
-                alloc sym (IStore _ (IType id _)) (st,e,p,f) =
+                alloc sym (IStore _ (IType id _)) (st,e,p,f) = 
                                 ([AllocBlock 1]
                                 ,setAddressById sym (st, storeEnvId env id)
-                                ,(st + 1,e, p + 1,f))
+                                ,(st + 1, e, p + 1,f))
 
-                alloc sym decl@(IFunc id params ret@(IStore _ d) glob loc cmd) (st,e,p,f) =
+                alloc sym decl@(IFunc id params ret@(IStore _ d) glob loc cmd) (st,e,p,f) = 
                                 (totinstr
                                 ,symf6
                                 ,(st,e, pt + 1,f))
                         where 
                             symf0 = setAddressById sym (p + 1, routineEnvId env id)    
 
-                            symf1 = addSymbolsImpl symf0 id id (paramsToDelc params)
-                            symf2 = addSymbolsImpl symf1 id id ret  
+                            symf1 = addSymbolsImpl symf0 id id (paramsToDelc params) 
+                            symf2 = addSymbolsImpl symf1 id id ret   
                             symf3 = addSymbolsImpl symf2 id id loc  
 
-                            (symf4, i) = paramsC symf3 id params
-                            symf5 = setAddressById symf4 (i,storeEnvId id $literalId d)
-                           
-                            (instLoc, symf6, store) = allocC symf5 id (st + 1,e,p,f) loc
-                            instCmd = cmdC symf6 id cmd pc
+                            (symf4, i) = paramsC symf3 id params 
+                            symf5 = setAddressById symf4 (i,storeEnvId id $literalId d) 
+                            
+                            (instLoc, symf6, store) = allocC symf5 id (st + 1,e,p,f) loc 
+                            instCmd = cmdC symf6 id store cmd pc
              
 
                             totinstr = [UncondJump (pt + 1)]  ++ instLoc ++ instCmd ++ [Return 1]
@@ -117,55 +124,60 @@ allocC sym env store decl =  alloc sym decl store
 
 literalId :: IDecl -> String
 literalId (IArrayType i _ _) = i
-literalId (IType i _)        = i
+literalId (IType i _)        = i 
 literalId _ = error "no type"
 
-declAllocC :: [Symbol] -> Env -> (IParameter,Int) -> ([Symbol], Int)
-declAllocC sym env (IParams  a b, addr) = declAllocC syma env (b, addra)        
-        where  (syma, addra) = declAllocC sym env (a, addr)
+declAllocC :: [Symbol] -> Env -> (IParameter,Int) -> ([Symbol], StackPointer)
+declAllocC sym env (IParams  a b, addr) = declAllocC syma env (b, addra)         
+        where  (syma, addra) = declAllocC sym env (a, addr) 
 
 declAllocC sym env (IParam _ _ _ lit, addr) = (setAddressById sym (addr, storeEnvId env $literalId lit), addr - 1)
 
-               
-paramsC :: [Symbol] -> Env -> IParameter -> ([Symbol], Int)
+                
+paramsC :: [Symbol] -> Env -> IParameter -> ([Symbol], StackPointer)
 paramsC sym env params = declAllocC sym env (params, -1)
 
 
-cmdC :: [Symbol] -> Env -> ICmd -> Int -> [Instruction]
-cmdC sym env (ICmds a b)  pc = (le ++ ri)
+cmdC :: [Symbol] -> Env -> Storage -> ICmd -> ProgramPointer -> [Instruction]
+cmdC sym env store (ICmds a b)  pc = le ++ ri
         where 
-                le = cmdC sym env a pc
-                ri = cmdC sym env b (pc + length le)
+                le = cmdC sym env store a pc
+                ri = cmdC sym env (addProgPointer store le) b (pc + length le)
 
-cmdC sym env ISkip  pc = []
-cmdC sym env (IBecomes l r)  pc =  oprL sym env l  ++ oprR sym env r ++ [Store]
-cmdC sym env (IIf expr cmd1 cmd2)  pc = inst
+cmdC sym env store ISkip  pc = []
+cmdC sym env store (IBecomes exl exr)  pc =  il  ++ ir ++ [Store]
+        where 
+                il = oprL sym env  pc exl
+                ir = oprR sym env  (pc + length il)  exr 
+
+
+cmdC sym env store (IIf expr cmd1 cmd2)  pc = inst
         where 
                 addrIf = pc + length expri + length ifi + 2
                 addrElse  = addrIf + length eli
 
-                expri = oprR sym env expr
-                ifi = cmdC sym env cmd1 (pc + length expri + 1)
-                eli = cmdC sym env cmd2 addrIf
-               
-                inst = expri ++ [CondJump addrIf] ++ ifi ++ [UncondJump addrElse] ++ eli
+                expri = oprR sym env pc expr
+                ifi = cmdC sym env store cmd1 (pc + length expri + 1)
+                eli = cmdC sym env store cmd2 addrIf
+                
+                inst = expri ++ [CondJump addrIf] ++ ifi ++ [UncondJump addrElse] ++ eli 
 
-cmdC sym env (IWhile expr cmd) pc = inst
+cmdC sym env store (IWhile expr cmd) pc = inst
         where
                 addrStart = pc
                 addrEnd   = pc + length expri + length loop + 2
 
-                expri   = oprR sym env expr
-                loop    = cmdC sym env cmd  (pc + length expri + 1)
+                expri   = oprR sym env pc expr 
+                loop    = cmdC sym env store cmd  (pc + length expri + 1) 
 
                 inst = expri ++ [CondJump addrEnd] ++ loop ++ [UncondJump addrStart]
 
 
-cmdC sym env (IDebugIn expr)  pc = oprL sym env expr  ++ [Input (IntTy WL1024) (Loc Nothing) ""]
-cmdC sym env (IDebugOut expr)  pc = oprR sym env expr  ++ [Output (IntTy WL1024) ""]
+cmdC sym env store (IDebugIn expr)  pc = oprL sym env pc expr  ++ [Input (IntTy WL1024) (Loc Nothing) ""]
+cmdC sym env store (IDebugOut expr)  pc = oprR sym env pc expr  ++ [Output (IntTy WL1024) ""]
 
 
-cmdC sym env b pc =  error $"cmdC " ++show b
+cmdC sym env store b pc =  error $"cmdC " ++show b
 
 
 addrerssByID sym id = case getSymbol sym id of 
@@ -175,63 +187,161 @@ addrerssByID sym id = case getSymbol sym id of
 
 
 
-oprL :: [Symbol] -> Env  -> IExpr -> [Instruction]
-oprL sym env (ILiteral id _) =
-        case findStoreSymbol sym env id of
+oprL :: [Symbol] -> Env -> ProgramPointer  -> IExpr -> [Instruction]
+oprL sym env store (ILiteral id _) = 
+          case findStoreSymbol sym env id of
                 ((_,_,_,_,addr), Glob) ->  [LoadIm IntVmTy (IntVmVal addr)]
                 ((_,_,_,_,addr), Local) -> [LoadAddrRel addr]
-                n -> error (show n)
-oprL sym env (ILiteralArray id expr)  =  []
-   
-
-oprL sym _ n  =  error $"no l value" ++ show n
 
 
-oprR :: [Symbol] -> Env -> IExpr -> [Instruction]
+oprL sym env pt (ILiteralArray id expr)  = case findStoreSymbol sym env id of
+        (n,_) -> let 
+                addr = getAddress n
+                addrLen = addr - 1
 
-oprR sym env (IExprList id expr) =
+                index = oprR sym env pt  expr 
+
+                overflow = index 
+                        ++ [LoadIm IntVmTy (IntVmVal addrLen)
+                           ,Deref
+                           ,Ge Int1024VmTy
+                           ,CondJump $pt + length index * 2 + 6]
+                        ++ index 
+                        ++ [Output (IntTy WL1024) "[Runtime Exception] array out of bounds"
+                           ,Stop]
+
+            in  
+                        overflow 
+                        ++ [LoadIm Int1024VmTy (Int1024VmVal $Int1024 $toInteger addr)] 
+                        ++ index
+                        ++ [Add Int1024VmTy (Loc Nothing)]
+                        ++ [Convert Int1024VmTy IntVmTy (Loc Nothing)]
+                -- ++ [LoadIm IntVmTy  (IntVmVal 0)] 
+
+ 
+
+
+
+
+oprL sym _ store n  =  error $"no l value" ++ show n
+
+
+oprR :: [Symbol] -> Env -> ProgramPointer -> IExpr -> [Instruction]
+
+oprR sym env pc (IExprList id expr) = 
         case findRoutineSymbol sym id of
-                Just n -> [AllocBlock 1] ++ oprR sym env expr ++ [Call (getAddress n)]
+                Just n -> [AllocBlock 1] ++ oprR sym env pc expr ++ [Call (getAddress n)]
                 Nothing -> error $"no function defined " ++ show (routineEnvId "" id) ++ "\n\n" ++ show sym
        
-       
-oprR sym env (IExprListParams a b) = oprR sym env  a ++ oprR sym env b
-oprR sym env (IOpr o e1 e2) = conc $opr' o
+        
+oprR sym env pc (IExprListParams a b) = ia ++ ib
+        where 
+                ia = oprR sym env pc  a 
+                ib = oprR sym env (pc + length ia)  b
+                
+
+oprR sym env pc (IOpr (LogicOperator OR )  a b) = inst
+        where 
+                ia = oprR sym env pc a 
+                ib = oprR sym env (pc + length ia)  b
+                
+                condAddr = pc +  length ia + 3
+                addr = condAddr  + length ib
+                inst = ia 
+                        ++ [CondJump condAddr
+                           ,LoadIm IntVmTy (IntVmVal 1)
+                           ,UncondJump addr
+                           ] 
+                        ++ ib
+                        
+
+oprR sym env pc (IOpr (LogicOperator AND)  a b) = inst
+        where 
+                ia = oprR sym env pc a 
+                ib = oprR sym env (pc + length ia)  b
+                
+                addr = pc +  length ia  +length ib + 2
+                inst = ia ++ [CondJump addr] 
+                          ++ ib
+                          ++ [UncondJump (addr + 1) 
+                             ,LoadIm IntVmTy (IntVmVal 0)]
+
+oprR sym env pc (IOpr o e1 e2) = conc $opr' o
      where
         opr' (AritmeticOperator PLUS)    = [Add Int1024VmTy (Loc Nothing)]
         opr' (AritmeticOperator MINUS )  = [Sub Int1024VmTy (Loc Nothing)]
         opr' (AritmeticOperator MULTI )  = [Mult Int1024VmTy (Loc Nothing)]
         opr' (AritmeticOperator DIV )    = [DivEuclid Int1024VmTy (Loc Nothing)]
-       
+        
         opr' (RelOperator EQUAL)          = [Eq Int1024VmTy]
         opr' (RelOperator NOT_EQUAL)      = [Ne Int1024VmTy]
         opr' (RelOperator GREATER)        = [Gt Int1024VmTy]
         opr' (RelOperator GREATER_EQUAL)  = [Ge Int1024VmTy]
         opr' (RelOperator LESS)           = [Lt Int1024VmTy]
         opr' (RelOperator LESS_EQUAL)     = [Le Int1024VmTy]
+        
 
-        opr' t = error $"no oprR" ++ show t
+        opr' t = error $"no oprR " ++ show t
 
-        conc op = oprR sym env e1  ++ oprR sym env e2  ++ op
+        conc op =  ia ++ ib ++ op
+                where 
+                        ia = oprR sym env pc  e1
+                        ib = oprR sym env (pc + length ia)  e2
 
-oprR sym env (IAliteral val) = [LoadIm Int1024VmTy (Int1024VmVal $Int1024 $toInteger val)]
-oprR sym env (ILiteral id _)   = inc
-       
-        where inc = case findStoreSymbol sym env id  of 
+oprR sym env pc (IAliteral val) = [LoadIm Int1024VmTy (Int1024VmVal $Int1024 $toInteger val)]
+oprR sym env pc (ILiteral id _)   = inc
+        
+     where inc = case findStoreSymbol sym env id  of 
                 ((_,_,_,_,addr),Glob)  -> [LoadIm IntVmTy (IntVmVal addr) ,Deref]
                 ((_,_,_,_,addr),Local) -> [LoadAddrRel addr, Deref]
-               
-                n -> error (show n)
 
-oprR sym env  n  = error $"no supported opr " ++ show n
+oprR sym env pc (IArrayLength id) =  
+        case findStoreSymbol sym env id of
+                (n,_) -> let 
+                        addrLen = getAddress n - 1
+                        in [LoadIm IntVmTy (IntVmVal addrLen) ,Deref]
+
+
+oprR sym env pt (ILiteralArray id expr) = case findStoreSymbol sym env id of
+        (n,_)-> let 
+                addr = getAddress n
+                addrLen = addr - 1
+
+                index = oprR sym env pt  expr 
+
+                overflow = index 
+                        ++ [LoadIm IntVmTy (IntVmVal addrLen)
+                           ,Deref
+                           ,Ge Int1024VmTy
+                           ,CondJump $pt + length index * 2 + 6]
+                        ++ index 
+                        ++ [Output (IntTy WL1024) "[Runtime Exception] array out of bounds"
+                           ,Stop]
+                        
+
+
+            in  
+                        overflow 
+                        ++ [LoadIm Int1024VmTy (Int1024VmVal $Int1024 $toInteger addr)] 
+                        ++ index
+                        ++ [Add Int1024VmTy (Loc Nothing)]
+                        ++ [Convert Int1024VmTy IntVmTy (Loc Nothing)]
+                        ++ [Deref ]
+                -- ++ [LoadIm IntVmTy  (IntVmVal 0)] 
+
+oprR sym env  store  n  = error $"no supported opr " ++ show n
 
 
 
-runProg :: [Instruction] -> IO ()
+
+
+runProg :: [Instruction] -> IO () 
 runProg isnt = do
+
         let n = listArray (0, length isnt - 1) isnt
 
         let prog = VMProgram (Ident "asdf",  n)
-        n <- debugProgram prog
+        debugProgram prog
         return ()
+
 
